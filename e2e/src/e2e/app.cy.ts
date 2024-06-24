d0fc0762c57e6dcdbea6f915f217ac4e6df8b1fe
Oleg Shilov <me@olegshilov.com>
2024-06-25 00:49:38 +0700

    test+ci: add small e2e test for main page, add gh workflows

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,15 @@
 import { getGreeting } from '../support/app.po';
 
 describe('e2e', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    return cy.visit('/');
+  });
 
-  it('should display welcome message', () => {
+  it('should display hero heading message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getGreeting().contains(/Home of ethical web3/);
   });
 });
